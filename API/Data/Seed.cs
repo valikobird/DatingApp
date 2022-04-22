5e215cc1ca78f286f654e293708cbadf467ae882
Valentyn Zozulia <valikobird@protonmail.com>
2022-04-22 13:22:11 -0500

    End of section 8

---
+++ API/Data/Seed.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class Seed
+{
+    public static async Task SeedUser(DataContext context)
+    {
+        if (await context.Users.AnyAsync())
+        {
+            return;
+        }
+
+        var userData = await File.ReadAllTextAsync("Data/UserSeedData.json");
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+        if (users == null)
+        {
+            return;
+        }
+
+        foreach (var user in users)
+        {
+            using var hmac = new HMACSHA512();
+            user.UserName = user.UserName.ToLower();
+            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("PaSSw0rd"));
+            user.PasswordSalt = hmac.Key;
+
+            context.Users.Add(user);
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
